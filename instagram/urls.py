07348cb9cf2a243f56e77e545b9d7a77988b82ed
Alex <alexserr.dev@gmail.com>
2024-11-07 18:40:03 +0000

    Adding Login Form and button for Logout session

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import HomeView, LoginView, RegisterView, LegalView, ContactView
+from .views import HomeView, LoginView, RegisterView, LegalView, ContactView, logout_view
 
 urlpatterns = [
     path('', HomeView.as_view(), name = 'home'),
     path('login/', LoginView.as_view(), name = 'login'),
+    path('logout/', logout_view, name='logout'),
     path('register/', RegisterView.as_view(), name = 'register'),
     path('contact/', ContactView.as_view(), name = 'contact'),
     path('legal/', LegalView.as_view(), name = 'legal'),
--- instagram/views.py
+++ instagram/views.py
@@ -1,20 +1,39 @@
-from django. shortcuts import render
+from django. shortcuts import render, HttpResponseRedirect
 
 from django.views.generic import TemplateView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, FormView
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
 
-from django.urls import reverse_lazy
-from .forms import RegistrationForm
+from django.urls import reverse_lazy, reverse
+from .forms import RegistrationForm, LoginForm
 
 
 class HomeView(TemplateView):
     template_name = "general/home.html"
 
 
-class LoginView(TemplateView):
+
+class LoginView(FormView):
     template_name = "general/login.html"
+    form_class = LoginForm
+
+    def form_valid(self, form):
+        usuario = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=usuario, password=password)
+
+        if user is not None:
+            login(self.request, user)
+            messages.add_message(self.request, messages.SUCCESS, f'Bienvenido de nuevo {user.username}')
+            return HttpResponseRedirect(reverse('home'))
+
+        else:
+            messages.add_message(
+                self.request, messages.ERROR, 'Usuario no valido o contraseña no valida')
+            return super(LoginView, self).form_invalid(form)
+    
 
 
 class RegisterView(CreateView):
@@ -30,9 +49,18 @@ class RegisterView(CreateView):
         return response # Retorna la respuesta para que se ejecute la redirección
 
 
+
 class LegalView(TemplateView):
     template_name = "general/legal.html"
 
 
+
 class ContactView(TemplateView):
     template_name = "general/contact.html"
+
+
+
+def logout_view(request):
+    logout(request)
+    messages.add_message(request, messages.INFO, "Se ha cerrado sesión correctamente.")
+    return HttpResponseRedirect(reverse('home'))
